8c10a44ed781c7672b50de7e8e5d9eec6b27a7c8
KosNet75 <nemokonstantin@gmail.com>
2022-09-18 09:22:38 +0700

    Two

--- src/Basket.java
+++ src/Basket.java
@@ -1,86 +1,145 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
-import java.util.Scanner;
+//import java.io.File;
+//import java.io.FileNotFoundException;
+//import java.io.FileWriter;
+//import java.io.IOException;
+//import java.io.PrintWriter;
+//import java.util.List;
+//import java.util.Scanner;
+//
+//
+//public class Basket {
+//
+//
+//  public static void addToCart(int[] tempAll, int prodCount, int num, int[] costAll,
+//      int[] writeProd,
+//      List<Integer> productCost, List<String> productSale, List<String> product)
+//      throws IOException {
+//
+//    writeProd[num] += prodCount;
+//    costAll[num] = tempAll[num] + productCost.get(num) * writeProd[num];
+//
+//    if (productSale.get(num).equals(product.get(num)) & writeProd[num] >= 3) {
+//      costAll[num] =
+//          tempAll[num] + (writeProd[num] - (writeProd[num] / 3)) * productCost.get(num);  // Акция
+//    }
+//
+//    saveTxt(productCost, costAll);
+//
+//  }
+//
+//
+//  private static void saveTxt(List<Integer> productCost, int[] costAll)
+//      throws IOException {
+//    int i;
+//    PrintWriter writer = new PrintWriter(new FileWriter("basket.txt"));
+//    for (i = 0; i < productCost.size(); i++) {
+//      String b = costAll[i] + "\n";
+//      writer.append(b);
+//    }
+//    writer.close();
+//  }
+//
+//
+//  protected static void loadFromTxtFile(int[] tempAll) throws FileNotFoundException {
+//    try (Scanner scanner = new Scanner(new File("basket.txt"))) {
+//      int temp = 0;
+//      while (scanner.hasNextLine()) {
+//        String line = scanner.nextLine();
+//        tempAll[temp] = Integer.parseInt(line);
+//        temp++;
+//
+//      }
+//
+//    }
+//
+//  }
+//
+//  public static void printCart(List<String> product, String[] productSale,
+//      int[] costAll, List<Integer> productCost) {
+//
+//    System.out.println("\n______________________________________________________");
+//    System.out.println("КОРЗИНА :");
+//
+//    for (int i = 0; i < product.size(); i++) {
+//      System.out.println(
+//          (i + 1) + ": " + product.get(i) + ": " + productCost.get(i) + " руб/кг(шт)." +
+//              "в корзине " + costAll[i] / productCost.get(i) + "кг/шт [по акции (" + productSale[i]
+//              + ")] на " + costAll[i] + " руб");
+//
+//    }
+//    int sumCost = 0;
+//
+//    for (int i = 0; i < product.size(); i++) {
+//
+//      sumCost += costAll[i];
+//    }
+//    System.out.println("______________________________________________________");
+//    System.out.println("Общая сумма покупок за все время составляет: " + sumCost + " руб.");
+//  }
+//
+//  public static class FalsePositionException extends RuntimeException {
+//
+//    public FalsePositionException() {
+//      super("Ошибка ввода величины позиции [1-5]");
+//    }
+//  }
+//}
 
 
-public class Basket {
 
+import java.io.*;
+import java.util.*;
 
-  public static void addToCart(int[] tempAll, int prodCount, int num, int[] costAll,
-      int[] writeProd,
-      List<Integer> productCost, List<String> productSale, List<String> product)
-      throws IOException {
+public class Basket implements Serializable {
 
-    writeProd[num] += prodCount;
-    costAll[num] = tempAll[num] + productCost.get(num) * writeProd[num];
+  private static final long SerialVersionUID = 1L;
+  private final Map<Integer, Integer> amountProduct;
 
-    if (productSale.get(num).equals(product.get(num)) & writeProd[num] >= 3) {
-      costAll[num] =
-          tempAll[num] + (writeProd[num] - (writeProd[num] / 3)) * productCost.get(num);  // Акция
-    }
+  private final List<Integer> prices;
+  private final List<String> products;
 
-    saveTxt(productCost, costAll);
+  public Basket(List<String> products, List<Integer> prices, Map<Integer, Integer> amountProduct) {
+    this.prices = prices;
+    this.products = products;
+    this.amountProduct = amountProduct;
+  }
+
+  public void addToCart(int productNum, int quantity) {
+    amountProduct.merge(productNum, quantity, Integer::sum);
 
   }
 
-
-  private static void saveTxt(List<Integer> productCost, int[] costAll)
-      throws IOException {
-    int i;
-    PrintWriter writer = new PrintWriter(new FileWriter("basket.txt"));
-    for (i = 0; i < productCost.size(); i++) {
-      String b = costAll[i] + "\n";
-      writer.append(b);
-    }
-    writer.close();
-  }
-
-
-  protected static void loadFromTxtFile(int[] tempAll) throws FileNotFoundException {
-    try (Scanner scanner = new Scanner(new File("basket.txt"))) {
-      int temp = 0;
-      while (scanner.hasNextLine()) {
-        String line = scanner.nextLine();
-        tempAll[temp] = Integer.parseInt(line);
-        temp++;
-
+  public void printCart() {
+    int All = 0;
+    for (int i = 0; i < products.size(); i++) {
+      if (amountProduct.get(i) != null) {
+        System.out.println(products.get(i) + " " + amountProduct.get(i) + "кг/шт  "
+            + prices.get(i) + " руб. за кг/шт     всего на: " + (amountProduct.get(i) * prices.get(i)) + "руб.");
+        All +=  (amountProduct.get(i) * prices.get(i));
       }
-
     }
-
+    System.out.println("Всего: " + All + " руб.");
   }
 
-  public static void printCart(List<String> product, String[] productSale,
-      int[] costAll, List<Integer> productCost) {
-
-    System.out.println("\n______________________________________________________");
-    System.out.println("КОРЗИНА :");
-
-    for (int i = 0; i < product.size(); i++) {
-      System.out.println(
-          (i + 1) + ": " + product.get(i) + ": " + productCost.get(i) + " руб/кг(шт)." +
-              "в корзине " + costAll[i] / productCost.get(i) + "кг/шт [по акции (" + productSale[i]
-              + ")] на " + costAll[i] + " руб");
-
-    }
-    int sumCost = 0;
-
-    for (int i = 0; i < product.size(); i++) {
-
-      sumCost += costAll[i];
-    }
-    System.out.println("______________________________________________________");
-    System.out.println("Общая сумма покупок за все время составляет: " + sumCost + " руб.");
-  }
-
-  public static class FalsePositionException extends RuntimeException {
-
-    public FalsePositionException() {
-      super("Ошибка ввода величины позиции [1-5]");
+  protected void saveBin(File file) throws IOException {
+    try (ObjectOutputStream writer = new ObjectOutputStream(new FileOutputStream(file))) {
+      writer.writeObject(this);
+      writer.flush();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
   }
+  public static Basket loadFromBinFile(File file) throws IOException, ClassCastException {
+    Basket load = null;
+    try (ObjectInputStream reader = new ObjectInputStream(new FileInputStream(file))) {
+      load = (Basket) reader.readObject();
+      load.printCart();
+    } catch (IOException io) {
+      throw new IOException(io);
+    } catch (ClassCastException | ClassNotFoundException cc) {
+      cc.printStackTrace();
+    }
+    return load;
+  }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,105 +1,109 @@
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+//import java.io.File;
+//import java.io.IOException;
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Scanner;
+//
+//public class Main {
+//
+//  public static void main(String[] args) throws IOException {
+//    Scanner scanner = new Scanner(System.in);
+//
+//    final int minNumOfLot = 1;
+//    List<String> product = new ArrayList<>(
+//        List.of(new String[]{"Яйцо", "Соль", "Хлеб", "Крупа", "Мясо"}));
+//    String[] productSale = {"Нет акции", "Соль", "Нет акции", "Крупа", "Нет акции"};
+//    List<Integer> productCost = new ArrayList<>(List.of(new Integer[]{80, 30, 35, 70, 150}));
+//
+//    int[] costAll = new int[productCost.size()];
+//    int[] writeProd = new int[product.size()];
+//    int[] tempAll = new int[product.size()];
+//
+//    System.out.println("____________________");
+//    System.out.println("В наличии продукты:");
+//
+//    for (int i = 0; i < product.size(); i++) {
+//      System.out.println(
+//          (i + 1) + ": " + product.get(i) + ": " + productCost.get(i) + " руб/кг(шт).");
+//
+//    }
+//    System.out.print("по Акции (3 кг/шт по цене 2 кг/шт):  ");
+//    for (String s : productSale) {
+//      if ("Нет акции".equals(s)) {
+//        continue;
+//      }
+//      System.out.print("[" + s + "]");
+//    }
+//    System.out.println("\nПосмотреть Корзину 'basket'\n" + "Выход и чек 'end'");
+//
+//    int num;
+//    int prodCount;
+//
+//    File f = new File("basket.txt");
+//    if (f.isFile()) {
+//      Basket.loadFromTxtFile(tempAll);
+//      for (int j = 0; j < costAll.length; j++) {
+//
+//        costAll[j] = tempAll[j] + costAll[j];
+//
+//      }
+//
+//    } else {
+//      System.out.println("Файла Корзины не существует! Покупка начнется с '0'!");
+//    }
+//
+//    while (true) {
+//
+//      try {
+//        System.out.print("\nВведите позицию покупаемого товара [1-5]: > ");
+//        String inputNum = scanner.nextLine();
+//
+//        if ("end".equals(inputNum)) {
+//          break;
+//        }
+//
+//        if ("basket".equals(inputNum)) {
+//          Basket.printCart(product, productSale, costAll, productCost);
+//          continue;
+//        }
+//
+//        if (Integer.parseInt(inputNum) < minNumOfLot
+//            || Integer.parseInt(inputNum) > product.size()) {
+//          System.out.println(new Basket.FalsePositionException());
+//          continue;
+//        }
+//
+//        System.out.print("Введите количество покупаемого: > ");
+//
+//        String inputLot = scanner.nextLine();
+//
+//        if ("basket".equals(inputLot)) {
+//          Basket.printCart(product, productSale, costAll, productCost);
+//          continue;
+//        }
+//
+//        if ("end".equals(inputLot)) {
+//          break;
+//        }
+//
+//        prodCount = Integer.parseInt(inputLot);
+//        num = Integer.parseInt(inputNum) - 1;
+//
+//        Basket.addToCart(tempAll, prodCount, num, costAll, writeProd, productCost,
+//            List.of(productSale), product);
+//
+//      } catch (NumberFormatException e) {
+//        System.out.println("Ошибка! Нужно вводить только числа!");
+//
+//      }
+//
+//    }
+//    Basket.printCart(product, productSale, costAll, productCost);
+//
+//    scanner.close();
+//  }
+//}
 
-public class Main {
 
-  public static void main(String[] args) throws IOException {
-    Scanner scanner = new Scanner(System.in);
 
-    final int minNumOfLot = 1;
-    List<String> product = new ArrayList<>(
-        List.of(new String[]{"Яйцо", "Соль", "Хлеб", "Крупа", "Мясо"}));
-    String[] productSale = {"Нет акции", "Соль", "Нет акции", "Крупа", "Нет акции"};
-    List<Integer> productCost = new ArrayList<>(List.of(new Integer[]{80, 30, 35, 70, 150}));
 
-    int[] costAll = new int[productCost.size()];
-    int[] writeProd = new int[product.size()];
-    int[] tempAll = new int[product.size()];
-
-    System.out.println("____________________");
-    System.out.println("В наличии продукты:");
-
-    for (int i = 0; i < product.size(); i++) {
-      System.out.println(
-          (i + 1) + ": " + product.get(i) + ": " + productCost.get(i) + " руб/кг(шт).");
-
-    }
-    System.out.print("по Акции (3 кг/шт по цене 2 кг/шт):  ");
-    for (String s : productSale) {
-      if ("Нет акции".equals(s)) {
-        continue;
-      }
-      System.out.print("[" + s + "]");
-    }
-    System.out.println("\nПосмотреть Корзину 'basket'\n" + "Выход и чек 'end'");
-
-    int num;
-    int prodCount;
-
-    File f = new File("basket.txt");
-    if (f.isFile()) {
-      Basket.loadFromTxtFile(tempAll);
-      for (int j = 0; j < costAll.length; j++) {
-
-        costAll[j] = tempAll[j] + costAll[j];
-
-      }
-
-    } else {
-      System.out.println("Файла Корзины не существует! Покупка начнется с '0'!");
-    }
-
-    while (true) {
-
-      try {
-        System.out.print("\nВведите позицию покупаемого товара [1-5]: > ");
-        String inputNum = scanner.nextLine();
-
-        if ("end".equals(inputNum)) {
-          break;
-        }
-
-        if ("basket".equals(inputNum)) {
-          Basket.printCart(product, productSale, costAll, productCost);
-          continue;
-        }
-
-        if (Integer.parseInt(inputNum) < minNumOfLot
-            || Integer.parseInt(inputNum) > product.size()) {
-          System.out.println(new Basket.FalsePositionException());
-          continue;
-        }
-
-        System.out.print("Введите количество покупаемого: > ");
-
-        String inputLot = scanner.nextLine();
-
-        if ("basket".equals(inputLot)) {
-          Basket.printCart(product, productSale, costAll, productCost);
-          continue;
-        }
-
-        if ("end".equals(inputLot)) {
-          break;
-        }
-
-        prodCount = Integer.parseInt(inputLot);
-        num = Integer.parseInt(inputNum) - 1;
-
-        Basket.addToCart(tempAll, prodCount, num, costAll, writeProd, productCost,
-            List.of(productSale), product);
-
-      } catch (NumberFormatException e) {
-        System.out.println("Ошибка! Нужно вводить только числа!");
-
-      }
-
-    }
-    Basket.printCart(product, productSale, costAll, productCost);
-
-    scanner.close();
-  }
-}
